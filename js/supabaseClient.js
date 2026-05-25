0a7efe643ad41fad9806ccebf16f179424808e8d
AutomatizaRussell <automatizacionmedellin@rbcol.co>
2026-05-25 19:06:48 +0000

    Fix Supa Url

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,5 +1,5 @@
 // js/supabaseClient.js
-const supabaseUrl = 'https://api.rbsupabase.rbgct.cloud:8000'; // La IP de tu VPS
+const supabaseUrl = 'https://api.rbsupabase.rbgct.cloud'; // La IP de tu VPS
 const supabaseKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTc3OTQ4ODk0MCwiZXhwIjo0OTM1MTYyNTQwLCJyb2xlIjoiYW5vbiJ9.LgDk68gU-GMfse0CHjrFsaDwl_SDzmiP4g5yytTDrcE';
 
 // Inicializamos Supabase globalmente
