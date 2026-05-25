9f1aa74a000dae946911be19cc2cfca91855fa7c
AutomatizaRussell <automatizacionmedellin@rbcol.co>
2026-05-25 18:05:46 +0000

    Final Update Coolify

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,5 +1,5 @@
 // js/supabaseClient.js
-const supabaseUrl = 'https://db.rbgct.cloud'; // La IP de tu VPS
+const supabaseUrl = 'https://rbsupabase.rbgct.cloud'; // La IP de tu VPS
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzExNTQ4MDAwLCJleHAiOjIyNDE0NDgwMDB9.j4VwYWqFcGOw8y4T0b4TlriPh35W8lrs_tuIuTrrrvQ';
 
 // Inicializamos Supabase globalmente
