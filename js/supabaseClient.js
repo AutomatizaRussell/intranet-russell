45acdcb1d48c163f1a9384abaa62ea1ee03ae329
AutomatizaRussell <automatizacionmedellin@rbcol.co>
2026-05-26 13:54:32 +0000

    Fix anon key error

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,6 +1,6 @@
 // js/supabaseClient.js
 const supabaseUrl = 'https://api.rbsupabase.rbgct.cloud'; // URL sin puerto
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzExNTQ4MDAwLCJleHAiOjIyNDE0NDgwMDB9.j4VwYWqFcGOw8y4T0b4TlriPh35W8lrs_tuIuTrrrvQ';
+const supabaseKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTc3OTQ4ODk0MCwiZXhwIjo0OTM1MTYyNTQwLCJyb2xlIjoiYW5vbiJ9.LgDk68gU-GMfse0CHjrFsaDwl_SDzmiP4g5yytTDrcE';
 
 // Inicializamos Supabase sin restricciones de esquema global
 window.supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
